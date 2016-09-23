50b9ccc659fc735c97ff3b87192b626b7ad261b4
Kane York <kyork@student.42.us.org>
2016-09-23 14:09:40 -0700

    fix build constraint?

--- console/didexit_dummy.go
+++ console/didexit_dummy.go
@@ -1,15 +1,15 @@
-// +build darwin !unix
+// +build darwin
 package console
 
 import (
-	"fmt"
 	"os"
 )
 
 func waitForExit(p *os.Process) chan struct{} {
 	ch := make(chan struct{})
 
-	fmt.Println("OS X does not support WNOWAIT")
+	panic("OS X does not support WNOWAIT")
+
 	close(ch)
 	return ch
 }
